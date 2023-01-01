7a2d3bdee974c1d4cd0944e04c6d43abefe174b1
Rohit Gupta <princerohit9893@gmail.com>
2023-01-02 04:50:07 +0530

    success

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const connect = require("./src/config/db");
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.send("Hello my");
+  return res.send("Hello my Love");
+});
+app.get("/rohit", (req, res) => {
+  return res.send("Hello my Rohit");
 });
 
 app.listen(8080, async () => {
