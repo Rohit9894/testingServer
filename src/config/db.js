525a862abf66b3a6d514365c8af3e30f1b502277
Rohit Gupta <princerohit9893@gmail.com>
2023-01-02 04:40:18 +0530

    test

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
 const connect = async () => {
   return mongoose.connect(
     "mongodb+srv://r:g@cluster0.ux5ulof.mongodb.net/?retryWrites=true&w=majority"
