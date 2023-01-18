26c675940262e1b0cc3bbacc6d92bfc452998a46
Rohit Gupta <princerohit9893@gmail.com>
2023-01-18 23:31:49 +0530

    hello

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 const connect = async () => {
   return mongoose.connect(
-    "mongodb+srv://r:g@cluster0.mvzw01d.mongodb.net/JioMart?retryWrites=true&w=majority"
-    {useNewUrlParser: "true",
-  useUnifiedTopology: "true"}
+    "mongodb+srv://r:g@cluster0.mvzw01d.mongodb.net/JioMart?retryWrites=true&w=majority",
+    { useNewUrlParser: "true", useUnifiedTopology: "true" }
   );
 };
 module.exports = connect;
